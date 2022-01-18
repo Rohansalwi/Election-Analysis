afbe4b44f8b40753c9ebda0da569a01f752fedc0
Rohansalwi <96554223+Rohansalwi@users.noreply.github.com>
2022-01-18 12:02:13 -0500

    Add files via upload

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -1,14 +1,11 @@
-# -*- coding: UTF-8 -*-
-"""PyPoll Homework Challenge Solution."""
-
 # Add our dependencies.
 import csv
 import os
 
 # Add a variable to load a file from a path.
-file_to_load = os.path.join("/Users/rohansalwi/Desktop/Assignment Projects/Python/Election-Analysis/Resources/Election-Analysis", "election_results.csv")
+file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("/Users/rohansalwi/Desktop/Assignment Projects/Python/Election-Analysis/Resources/Election-Analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
